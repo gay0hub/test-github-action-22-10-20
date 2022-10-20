3da0b438e64107af08abf460640facbcbca027a4
iamqiz <zq20491001@163.com>
2022-10-20 10:55:36 +0800

    py脚本把命令行参数输出到文件

--- py1.py
+++ py1.py
@@ -4,5 +4,7 @@
 import sys
 
 if __name__ == '__main__':
-    for i in range(len(sys.argv)):
-        print(i,sys.argv[i])
+    with open('tmp-out.txt',"w",encoding="utf8") as outf:
+        for i in range(len(sys.argv)):
+            print(i,sys.argv[i])
+            outf.write(f"{i},{sys.argv[i]}\n")
