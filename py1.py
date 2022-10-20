3348efe856eea8477f3cbb09f09a2634bd9a5514
iamqiz <zq20491001@163.com>
2022-10-20 11:39:24 +0800

    打印secrets.GITHUB_TOKEN

--- py1.py
+++ py1.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == '__main__':
     with open('tmp-out.txt',"w",encoding="utf8") as outf:
         for i in range(len(sys.argv)):
-            print(i,sys.argv[i])
+            print(i,sys.argv[i],"拆分<"+'-'.join(list(sys.argv[i]))+">")
             outf.write(f"{i},{sys.argv[i]}\n")
         outf.write(f"is:{sys.argv[-1]=='***'}\n")
         outf.write(f"is-nmsl:{sys.argv[-1]=='nmsl'}\n")
