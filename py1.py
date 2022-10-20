33981eb93a038cbe57966eef12cbf9e52e616b0b
iamqiz <zq20491001@163.com>
2022-10-20 11:05:01 +0800

    判断py参数是否是特定值

--- py1.py
+++ py1.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
             print(i,sys.argv[i])
             outf.write(f"{i},{sys.argv[i]}\n")
         outf.write(f"is:{sys.argv[-1]=='***'}\n")
+        outf.write(f"is-nmsl:{sys.argv[-1]=='nmsl'}\n")
